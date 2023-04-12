0dc0981d813c88437436c32e0cc2ca702e79df30
Fernando Gnadt de Oliveira <fernando.oliveira@mobilus.com.br>
2023-04-12 17:44:04 -0300

    add migrations cliente, estab and admins

--- start/routes.ts
+++ start/routes.ts
@@ -1,37 +1,31 @@
 import Route from "@ioc:Adonis/Core/Route";
-import User from "App/Models/User";
-import type { HttpContextContract } from "@ioc:Adonis/Core/HttpContext";
+// import User from "App/Models/User";
+// import type { HttpContextContract } from "@ioc:Adonis/Core/HttpContext";
 
 Route.get("/", async () => {
-  User.create({
-    email: "admin@email.com",
-    password: "123456",
-    tipo: "admin",
-  });
+  return {
+    hortifruti: "prático",
+  };
 });
 
-Route.post(
-  "/gettoken",
-  async ({ request, response, auth }: HttpContextContract) => {
-    const email = request.input("email");
-    const password = request.input("password");
+// Route.post(
+//   "/gettoken",
+//   async ({ request, response, auth }: HttpContextContract) => {
+//     const email = request.input("email");
+//     const password = request.input("password");
 
-    const user = await User.findBy("email", email);
+//     const user = await User.findBy("email", email);
 
-    if (user == null) {
-      return response.notFound("Usuário não encontrado");
-    }
+//     if (user == null) {
+//       return response.notFound("Usuário não encontrado");
+//     }
 
-    const token = await auth.use("api").attempt(email, password);
+//     const token = await auth.use("api").attempt(email, password);
 
-    return response.ok(token);
-  }
-);
+//     return response.ok(token);
+//   }
+// );
 
-Route.get("/auth", async ({ auth, response }: HttpContextContract) => {
-  const userAuth = await auth.use("api").authenticate();
-
-  if (userAuth) {
-    return response.ok(userAuth);
-  }
-});
+// Route.get("/auth", async ({ response }: HttpContextContract) => {
+//   return response.ok("Somente usuários autenticados podem acessar");
+// }).middleware("auth");
