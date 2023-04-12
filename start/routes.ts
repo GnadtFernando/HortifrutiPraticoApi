31f6671e46ec2d4302b1d98a59acb218e1e7cf22
Fernando Gnadt de Oliveira <fernando.oliveira@mobilus.com.br>
2023-04-12 17:13:31 -0300

    add get token and auth

--- start/routes.ts
+++ start/routes.ts
@@ -1,5 +1,6 @@
 import Route from "@ioc:Adonis/Core/Route";
 import User from "App/Models/User";
+import type { HttpContextContract } from "@ioc:Adonis/Core/HttpContext";
 
 Route.get("/", async () => {
   User.create({
@@ -7,4 +8,30 @@ Route.get("/", async () => {
     password: "123456",
     tipo: "admin",
   });
+});
+
+Route.post(
+  "/gettoken",
+  async ({ request, response, auth }: HttpContextContract) => {
+    const email = request.input("email");
+    const password = request.input("password");
+
+    const user = await User.findBy("email", email);
+
+    if (user == null) {
+      return response.notFound("Usuário não encontrado");
+    }
+
+    const token = await auth.use("api").attempt(email, password);
+
+    return response.ok(token);
+  }
+);
+
+Route.get("/auth", async ({ auth, response }: HttpContextContract) => {
+  const userAuth = await auth.use("api").authenticate();
+
+  if (userAuth) {
+    return response.ok(userAuth);
+  }
 });
