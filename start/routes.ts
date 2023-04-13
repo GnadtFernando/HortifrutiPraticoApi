517d114b12e7ae108b3fc30fd3f00eb57951f7a2
Fernando Gnadt de Oliveira <fernando.oliveira@mobilus.com.br>
2023-04-12 21:03:40 -0300

    cadastro e edeiçao de cliente

--- start/routes.ts
+++ start/routes.ts
@@ -4,6 +4,15 @@ import Route from "@ioc:Adonis/Core/Route";
 Route.post("/login", "AuthController.login");
 Route.post("/logout", "AuthController.logout");
 
+// Cadastra user cliente
+Route.post("/cliente/cadastro", "ClientesController.store");
+
+Route.group(() => {
+  Route.get("/auth/me", "AuthController.me");
+
+  Route.put("/cliente/editar", "ClientesController.update");
+}).middleware("auth");
+
 Route.get("/", async () => {
   return {
     hortifruti: "prático",
