6d3c378ab4e1771a954f582a23eafb6f4bfb0005
Fernando Gnadt de Oliveira <fernando.oliveira@mobilus.com.br>
2023-04-12 22:01:36 -0300

    cidades e estados

--- start/routes.ts
+++ start/routes.ts
@@ -7,6 +7,15 @@ Route.post("/logout", "AuthController.logout");
 // Cadastra user cliente
 Route.post("/cliente/cadastro", "ClientesController.store");
 
+// Todas as cidades
+Route.get("/cidades", "CidadesController.index");
+
+//Todas as cidades com estabelecimentos
+Route.get(
+  "/cidades/:id/estabelecimentos",
+  "CidadesController.estabelecimentos"
+);
+
 Route.group(() => {
   Route.get("/auth/me", "AuthController.me");
 
